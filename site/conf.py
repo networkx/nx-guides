0775cb509ca6b1bfcdd27737be367626ac6a5077
Ross Barnowski <rossbar@berkeley.edu>
2021-04-14 18:24:14 -0700

    Set up sphinx-book-theme.

--- site/conf.py
+++ site/conf.py
@@ -45,7 +45,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
+html_title = 'NetworkX Notebooks'
+html_logo = '_static/networkx_logo.svg'
+# html_favicon
+html_theme_options = {
+    "github_url": "https://github.com/networkx/notebooks/",
+    "repository_url": "https://github.com/networkx/notebooks/",
+    "repository_branch": "main",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "path_to_docs": "site/",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+#        "jupyterhub_url": 
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
